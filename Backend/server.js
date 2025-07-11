cb07db3e42a8e93bb81214cbf7f25b16181e3a78
mehansh03 <mehanshmasih03@gmail.com>
2025-07-11 23:03:39 +0530

    added initial Express server setup with routes and MongoDB connection

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const userRoutes = require('./routes/userRoutes');
 const laptopRoutes = require('./routes/laptopRoutes');
+const cors = require('cors');
+require('./models/user');
+require('./models/Laptop');
+
+
 require('dotenv').config();
 
 const app = express();
 connectDB();
 
-app.use(express.json());
+app.use(cors());
+app.use(express.json()); 
+
+// Routes
+app.use('/api/users', userRoutes);
 app.use('/api/laptops', laptopRoutes);
 
 const PORT = process.env.PORT || 5000;
